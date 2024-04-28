c32680588bbee6a56e1af278b1d227b46024054e
tabarra <1808295+tabarra@users.noreply.github.com>
2024-04-28 15:23:03 -0300

    added icon + small changes

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ It is hardcoded to use CTRL+WIN+[0~9] to switch between virtual desktops, and th
 Also worth mentioning that I am not a C# developer, so the code is also unapologetically imperfect.
 
 # Contributing & License
-I am open to improving this project if others show interest in using it.  
-This project is licensed under the [NoFreeRide License](./LICENSE).
+- I am open to improving this project if others show interest in using it.  
+- This project is licensed under the [NoFreeRide License](./LICENSE).
+- [App Icon](https://www.flaticon.com/free-icon/computer_13484563) made by __Muhammad Atif__ from [www.flaticon.com](https://www.flaticon.com);
--- src/InfoForm.Designer.cs
+++ src/InfoForm.Designer.cs
@@ -28,52 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InfoForm));
             label1 = new Label();
             linkLabel1 = new LinkLabel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(50, 9);
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(9, 12);
             label1.Name = "label1";
-            label1.Size = new Size(220, 44);
+            label1.Size = new Size(316, 72);
             label1.TabIndex = 0;
-            label1.Text = "Use CTRL+WIN+[0~9] to\r\nswitch Virtual Desktops";
-            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Text = "Use CTRL+WIN+[0~9] to switch Virtual Desktops";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // linkLabel1
             // 
             linkLabel1.AutoSize = true;
-            linkLabel1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            linkLabel1.Location = new Point(16, 78);
+            linkLabel1.Dock = DockStyle.Fill;
+            linkLabel1.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            linkLabel1.Location = new Point(9, 84);
             linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(288, 17);
+            linkLabel1.Size = new Size(316, 15);
             linkLabel1.TabIndex = 1;
             linkLabel1.TabStop = true;
             linkLabel1.Text = "https://github.com/tabarra/txDeskSwitcher";
+            linkLabel1.TextAlign = ContentAlignment.MiddleCenter;
             linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(linkLabel1, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.Padding = new Padding(6, 12, 6, 12);
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(334, 111);
+            tableLayoutPanel1.TabIndex = 2;
             // 
             // InfoForm
             // 
             AutoScaleDimensions = new SizeF(11F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(318, 104);
-            Controls.Add(linkLabel1);
-            Controls.Add(label1);
+            ClientSize = new Size(334, 111);
+            Controls.Add(tableLayoutPanel1);
             Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Margin = new Padding(5, 4, 5, 4);
             MaximizeBox = false;
             Name = "InfoForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "txDeskSwitcher";
             FormClosing += InfoForm_FormClosing;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
         private LinkLabel linkLabel1;
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
